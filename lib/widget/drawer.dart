197c0cd26a7fd89ac86b25734342ad1b171aeac2
Nayab <nayabnaveed046>
2021-01-08 22:10:32 -0800

    Update notes using SQL_file

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -8,68 +8,62 @@ import 'package:url_launcher/url_launcher.dart';
 
 import 'drawer_filter.dart';
 
-
 class AppDrawer extends StatelessWidget {
   @override
-  Widget build(BuildContext context) =>
-      Consumer<NoteFilter>(
-        builder: (context, filter, _) =>
-            Drawer(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  _drawerHeader(context),
-                  if (isNotIOS) const SizedBox(height: 25),
-                  DrawerFilterItem(
-                    icon: AppIcons.thumbtack,
-                    title: 'Notes',
-                    isChecked: filter.noteState == NoteState.unspecified,
-                    onTap: () {
-                      filter.noteState = NoteState.unspecified;
-                      Navigator.pop(context);
-                    },
-                  ),
-
-                  const Divider(),
-                  DrawerFilterItem(
-                    icon: AppIcons.archive_outlined,
-                    title: 'Archive',
-                    isChecked: filter.noteState == NoteState.archived,
-                    onTap: () {
-                      filter.noteState = NoteState.archived;
-                      Navigator.pop(context);
-                    },
-                  ),
-                  DrawerFilterItem(
-                    icon: AppIcons.delete_outline,
-                    title: 'Trash',
-                    isChecked: filter.noteState == NoteState.deleted,
-                    onTap: () {
-                      filter.noteState = NoteState.deleted;
-                      Navigator.pop(context);
-                    },
-                  ),
-                  const Divider(),
-                  DrawerFilterItem(
-                    icon: AppIcons.settings_outlined,
-                    title: 'Settings',
-                    onTap: () {
-                      Navigator.popAndPushNamed(context, '/settings');
-                    },
-                  ),
-                  DrawerFilterItem(
-                    icon: Icons.help_outline,
-                    title: 'About',
-                    onTap: () =>
-                        launch('https://github.com/xinthink/flutter-keep'),
-                  ),
-                ],
+  Widget build(BuildContext context) => Consumer<NoteFilter>(
+        builder: (context, filter, _) => Drawer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _drawerHeader(context),
+              if (isNotIOS) const SizedBox(height: 25),
+              DrawerFilterItem(
+                icon: AppIcons.thumbtack,
+                title: 'Notes',
+                isChecked: filter.noteState == NoteState.unspecified,
+                onTap: () {
+                  filter.noteState = NoteState.unspecified;
+                  Navigator.pop(context);
+                },
               ),
-            ),
+              const Divider(),
+              DrawerFilterItem(
+                icon: AppIcons.archive_outlined,
+                title: 'Archive',
+                isChecked: filter.noteState == NoteState.archived,
+                onTap: () {
+                  filter.noteState = NoteState.archived;
+                  Navigator.pop(context);
+                },
+              ),
+              DrawerFilterItem(
+                icon: AppIcons.delete_outline,
+                title: 'Trash',
+                isChecked: filter.noteState == NoteState.deleted,
+                onTap: () {
+                  filter.noteState = NoteState.deleted;
+                  Navigator.pop(context);
+                },
+              ),
+              const Divider(),
+              DrawerFilterItem(
+                icon: AppIcons.settings_outlined,
+                title: 'Settings',
+                onTap: () {
+                  Navigator.popAndPushNamed(context, '/settings');
+                },
+              ),
+              DrawerFilterItem(
+                icon: Icons.help_outline,
+                title: 'About',
+                onTap: () => launch('https://github.com/xinthink/flutter-keep'),
+              ),
+            ],
+          ),
+        ),
       );
 
-  Widget _drawerHeader(BuildContext context) =>
-      SafeArea(
+  Widget _drawerHeader(BuildContext context) => SafeArea(
         child: Container(
           padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
           child: RichText(
